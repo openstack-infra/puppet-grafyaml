3bbab2cd67da7b38394ec7782c281f433ba03766
Paul Belanger <pabelanger@redhat.com>
2015-10-07 14:03:19 -0400

    Initial cookiecutter import
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: grafyaml
+#
+# Full description of class grafyaml here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class grafyaml {}
